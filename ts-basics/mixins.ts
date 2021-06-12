a6a88776fb9c8ae1ad3eb04c2cebb7411cde953b
iuliancarnaru <iulian.carnaru@gmail.com>
2021-06-12 18:49:28 +0100

    challenge#3

---
+++ ts-basics/mixins.ts
@@ -0,0 +1,64 @@
+function myLogFunction() {
+  return (str: string) => {
+    console.log(str);
+  };
+}
+
+const logger = myLogFunction();
+logger('my string');
+
+function createLoggerClass() {
+  return class MyLoggerClass {
+    private completeLog: string = '';
+
+    log(str: string) {
+      this.completeLog += str + '\n';
+    }
+
+    dumpLog() {
+      return this.completeLog;
+    }
+  };
+}
+
+const MyLogger = createLoggerClass();
+const logger2 = new MyLogger();
+logger2.log('foo');
+console.log(logger2.dumpLog());
+
+function CreateSimpleMemoryDatabase<T>() {
+  return class SimpleMemoryDatabase {
+    private db: Record<string, T> = {};
+
+    set(id: string, value: T) {
+      this.db[id] = value;
+    }
+
+    get(id: string): T {
+      return this.db[id];
+    }
+
+    getObject(): object {
+      return this.db;
+    }
+  };
+}
+
+const StringDatabase = CreateSimpleMemoryDatabase<string>();
+const sbd1 = new StringDatabase();
+sbd1.set('foo', 'baz');
+
+type Constructor<T> = new (...args: any[]) => T;
+
+function Dumpable<T extends Constructor<{ getObject(): object }>>(Base: T) {
+  return class Dumpable extends Base {
+    dump() {
+      console.log(this.getObject());
+    }
+  };
+}
+
+const DumpableStringDatabase = Dumpable(StringDatabase);
+const sbd2 = new DumpableStringDatabase();
+sbd2.set('foo', 'baz');
+sbd2.dump();
